919f20787a2303569b70354902ee31f2a3ea99b2
firew <firewfilpos27@gmail.com>
2025-03-13 16:15:31 -0400

    creating routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\LoginRegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('products', ProductController::class);
+
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+});
